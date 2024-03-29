96cb038927d6eceaec0c33e5d315725a09f16912
LiteEze <liteversion09@gmail.com>
2024-03-29 14:20:49 +0100

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# installing a package using puppet
-
-package { 'flask':
-  ensure   => 'latest',
-  provider => 'pip3'
-}
+# install flask from pip3
+package { 'flask' :
+    ensure   => '2.1.0',
+    provider => 'pip3',
+    }
